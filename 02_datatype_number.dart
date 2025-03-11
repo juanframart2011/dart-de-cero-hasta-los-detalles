c3701a6ca39db7d67d56f981437688f6f8fd88c8
juanframart2011 <juanframart2011@gmail.com>
2025-03-10 22:43:13 -0600

    type number

---
+++ 02_datatype_number.dart
@@ -0,0 +1,18 @@
+main(){
+
+  // == Números
+  int a = 10;
+  double b = 5.5;
+  int? c;
+
+  double resultado = a + b;
+
+  print(c);
+  _suma( a, a );
+  print(c);
+}
+
+_suma( a, b ){
+
+  print( a + b );
+}
