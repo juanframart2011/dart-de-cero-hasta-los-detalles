e5895fdf861f5670dea5e3362afeaa219abd3c99
juanframart2011 <juanframart2011@gmail.com>
2025-03-28 22:46:25 -0600

    init section 10

---
+++ section_10_http/bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:section_10_http/section_10_http.dart' as section_10_http;
+
+void main(List<String> arguments) {
+  print('Hello world: ${section_10_http.calculate()}!');
+}
---
+++ section_10_http/test/section_10_http_test.dart
@@ -0,0 +1,8 @@
+import 'package:section_10_http/section_10_http.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
